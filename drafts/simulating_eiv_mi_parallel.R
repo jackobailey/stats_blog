0f6ab28dfa943ce546ca5a129725fb6981fcd16e
jon <jonamellon@gmail.com>
2023-07-03 10:41:30 -0400

    latest simulations

---
+++ drafts/simulating_eiv_mi_parallel.R
@@ -0,0 +1,345 @@
+set.seed(43)
+n.sims <- 100
+library(mirt)
+library(mitools)
+library(tidyverse)
+alpha <- 1
+beta_1 <- 1
+beta_2 <- 1
+n <- 2000
+n.cats <- 2
+beta_cat <- 0.5
+x1.x2.beta <- 0.5
+
+include.pv <- TRUE
+sim.out <- as.list(rep(NA, n.sims)) 
+
+
+simMIEIV <- function(sim, include.pv = TRUE, 
+                     alpha = 1,
+                     beta_1 = 1,
+                     beta_2 = 1,
+                     n = 2000,
+                     n.cats = 2,
+                     beta_cat = 0.5,
+                     x1.x2.beta = 0.5) {
+  print(sim)
+  cat <- as.numeric(sample(1:n.cats, n, replace = T)==1)
+  x1 <- rnorm(n) 
+  x1 <- scale(x1)[, ]
+  x2 <- rnorm(n)  + x1.x2.beta*x1 + beta_cat * cat
+  x2 <- scale(x2)[, ]
+  
+  
+  y <- alpha + beta_1 * x1 + beta_2 * x2  + rnorm(n, sd = 1)
+  
+  x1a <- rbinom(prob = plogis(x1 + rnorm(n, sd=1)+1), size = 1, n=n)
+  x1b <- rbinom(prob = plogis(x1 *0.5+ rnorm(n, sd=2)-1), size = 1, n=n)
+  x1c <- rbinom(prob = plogis(x1 *2), size = 1, n=n)
+  x1d <- rbinom(prob = plogis(x1 * 1.5 + rnorm(n, sd=2)+0.25), size = 1, n=n)
+  x1e <- rbinom(prob = plogis(x1 *0.1 + rnorm(n, sd=1)), size = 1, n=n)
+  x1f <- rbinom(prob = plogis(x1 *1 + rnorm(n, sd=0.5)), size = 1, n=n)
+  x1g <- rbinom(prob = plogis(x1 *1 + rnorm(n, sd=0.5)), size = 1, n=n)
+  x1h <- rbinom(prob = plogis(x1 *2 + rnorm(n, sd=0.5)), size = 1, n=n)
+  
+  x2a <- rbinom(prob = plogis(x2 + rnorm(n, sd=1)+1), size = 1, n=n)
+  x2b <- rbinom(prob = plogis(x2 *0.5+ rnorm(n, sd=2)-1), size = 1, n=n)
+  x2c <- rbinom(prob = plogis(x2 *2), size = 1, n=n)
+  x2d <- rbinom(prob = plogis(x2 + rnorm(n, sd = 5)), size = 1, n=n)
+  
+  x1dat <- data.frame(x1a,x1b, x1c, x1d, x1e, x1f, x1g, x1h)
+  x2dat <- data.frame(x2a,x2b, x2c, x2d)
+  x1.mod <- mirt(data = x1dat,
+                 model = 1, verbose = FALSE)
+  x2.mod <- mirt(data = x2dat, 
+                 model = 1, verbose = FALSE)
+  x1.hat <- fscores(x1.mod, full.scores= TRUE, full.scores.SE = TRUE, method = "EAP")
+  x2.hat <- fscores(x2.mod, full.scores= TRUE, full.scores.SE = TRUE, method = "EAP")
+  
+  reliabilities <- c(x1 = as.vector(empirical_rxx(x1.hat)),
+                     x2 = as.vector(empirical_rxx(x2.hat)))
+  
+  reliabilities2 <- c(x1 = as.vector(marginal_rxx(x1.mod)),
+                      x2 = as.vector(marginal_rxx(x2.mod)))
+  
+  rhoToS <- function(rho) {
+    s <- sqrt((1 / rho)^2 - 1)  
+    return(s)
+  }
+  
+  
+  rho.x1.est <- sqrt(empirical_rxx(x1.hat))
+  rho.x2.est <- sqrt(empirical_rxx(x2.hat))
+  
+  rho.x1.est.m <- sqrt(marginal_rxx(x1.mod))
+  rho.x2.est.m <- sqrt(marginal_rxx(x2.mod))
+  
+  rho.x1.true <- cor(cbind(x1.hat[, 1], x1))[1,2]
+  rho.x2.true <- cor(cbind(x2.hat[, 1], x2))[1,2]
+  
+  data.hat.empirical <- data.frame(y, x1 = (x1.hat[, 1]/ sd(x1.hat[, 1])) * sqrt((1+rhoToS(rho.x1.est)^2)),
+                                   x2 = (x2.hat[, 1]/ sd(x2.hat[, 1])) *  sqrt (1+rhoToS(rho.x2.est)^2), 
+                                   rxx1 = rho.x1.est^2,
+                                   rxx2 = rho.x2.est^2,
+                                   r.type = "empirical")
+  
+  data.hat.marginal <- data.frame(y, x1 = (x1.hat[, 1]/ sd(x1.hat[, 1])) * sqrt((1+rhoToS(rho.x1.est.m)^2)),
+                                  x2 = (x2.hat[, 1]/ sd(x2.hat[, 1])) *  sqrt (1+rhoToS(rho.x2.est.m)^2), 
+                                  rxx1 = rho.x1.est.m^2,
+                                  rxx2 = rho.x2.est.m^2,
+                                  r.type = "marginal")
+  
+  data.hat.real <- data.frame(y, x1 = (x1.hat[, 1]/ sd(x1.hat[, 1])) * sqrt((1+rhoToS(rho.x1.true)^2)),
+                              x2 = (x2.hat[, 1]/ sd(x2.hat[, 1])) *  sqrt (1+rhoToS(rho.x2.true)^2), 
+                              rxx1 = rho.x1.true^2,
+                              rxx2 = rho.x2.true^2,
+                              r.type = "real")
+  data.hat.irt <- data.frame(y, x1 = x1.hat[, 1],
+                             x2 = x2.hat[, 1], 
+                             rxx1 = rho.x1.est.m^2,
+                             rxx2 = rho.x2.est.m^2,
+                             r.type = "marginal")
+  data.classical <- data.frame(y, 
+                               x1 = x1 + rnorm(n, sd = rhoToS(rho.x1.est)) ,
+                               x2 = x2 + rnorm(n, sd = rhoToS(rho.x2.est)), 
+                               rxx1 = rho.x1.est^2,
+                               rxx2 = rho.x2.est^2,
+                               r.type = "simulated")
+  data.classical2 <- data.classical
+  
+  data.classical2$rxx1 <- cor(cbind(data.classical$x1, x1))[1,2]^2
+  data.classical2$rxx2 <- cor(cbind(data.classical$x2, x2))[1,2]^2
+  data.classical2$r.type <- "real"
+  
+  library(eivtools)
+  
+  datasets <- list(classical = data.classical,
+                   classical.real = data.classical2,
+                   irt = data.hat.irt, 
+                   irt.cool.empirical = data.hat.empirical,
+                   irt.cool.marginal = data.hat.marginal,
+                   irt.cool.real = data.hat.real)
+  
+  temp.sim.res <- list()
+  for(dat.name in names(datasets)) {
+    lm.mod.temp <- lm(data = datasets[[dat.name]], 
+                      formula = y~x1+x2)
+    # using empirical rxx 
+    eirt.mod.temp <- eivreg(data = datasets[[dat.name]], 
+                            formula = y~x1+x2, 
+                            reliability = c(x1 = datasets[[dat.name]]$rxx1[1], 
+                                            x2 = datasets[[dat.name]]$rxx2[2]))
+    
+    ols.temp.out <- data.frame(var = names(lm.mod.temp$coefficients),
+                               coefficient = lm.mod.temp$coefficients, 
+                               se = summary(lm.mod.temp)$coefficients[, "Std. Error"] , 
+                               error = dat.name, 
+                               estimator = "OLS", 
+                               r.method = "none",
+                               reliability = NA)
+    
+    eiv.temp.out <- data.frame(var = names(lm.mod.temp$coefficients),
+                               coefficient = eirt.mod.temp$coefficients, 
+                               se = summary(eirt.mod.temp)$coefficients[, "Std. Error"], 
+                               error = dat.name, 
+                               estimator = "EIV", 
+                               r.method = datasets[[dat.name]]$r.type[1],
+                               reliability = eirt.mod.temp$reliability[names(lm.mod.temp$coefficients)])
+    
+    dat.temp.out <- rbind(ols.temp.out, eiv.temp.out)
+    true.cors <- c(x1 = cor(cbind(x1, datasets[[dat.name]]$x1))[1,2], 
+                   x2 = cor(cbind(x2, datasets[[dat.name]]$x2))[1,2])
+    dat.temp.out$true.cor.latent <- true.cors[dat.temp.out$var]
+    temp.sim.res[[dat.name]] <-   dat.temp.out
+  }
+  
+  
+  
+  if(include.pv) {
+    bothdat <- cbind(x1dat, x2dat)
+    sim_data <- cbind(y, bothdat)
+    mod.text <- paste0("F1 = 1-", ncol(x1dat), "\n", 
+                       "F2 = ", (ncol(x1dat)+1), "-",
+                       ncol(bothdat), "\n",
+                       "COV = F1*F2")
+    model <- mirt.model(mod.text)
+    comb.mod <- mirt(data = bothdat, 
+                     model = model, 
+                     covdata = data.frame(y), 
+                     formula = ~.)
+    
+    pv_ycov <- fscores(comb.mod,  plausible.draws = 50)
+    
+    
+    draw_merger_ycov <- function(draw_num = 1){
+      sim_data_draw <- sim_data %>% 
+        mutate(x1 = pv_ycov[[draw_num]][,1] / sd(pv_ycov[[draw_num]][,1]), 
+               x2 = pv_ycov[[draw_num]][,2] / sd(pv_ycov[[draw_num]][,2]))
+      return(sim_data_draw)
+    }
+    pv_imputes_ycov <- map(1:length(pv_ycov), draw_merger_ycov) %>%
+      imputationList()
+    mi_mods_ycov <- with(pv_imputes_ycov,  lm(y ~ x1 + x2))
+    pv.mod <- MIcombine(mi_mods_ycov)
+    
+    pv.temp.out <- data.frame(var = names(lm.mod.temp$coefficients),
+                              coefficient = pv.mod$coefficients, 
+                              se = diag(sqrt(pv.mod$variance)), 
+                              error = "PV: covariates + covariance", 
+                              estimator = "MI", 
+                              r.method = "None",
+                              reliability = NA)
+    pv.temp.out$true.cor.latent <- true.cors[pv.temp.out$var]
+    temp.sim.res[["pvfull"]] <- pv.temp.out
+  }
+  
+  if(include.pv) {
+    bothdat <- cbind(x1dat, x2dat)
+    sim_data <- cbind(y, bothdat)
+    
+    x1.mod.ycov <- mirt(data = x1dat,
+                   model = 1, verbose = FALSE, 
+                   covdata = data.frame(y), 
+                   formula = ~.)
+    x2.mod.ycov <- mirt(data = x2dat, 
+                   model = 1, verbose = FALSE,
+                   covdata = data.frame(y), 
+                   formula = ~.)
+    pv_x1.ycov <- fscores(x1.mod.ycov,  plausible.draws = 50)
+    pv_x2.ycov <- fscores(x2.mod.ycov,  plausible.draws = 50)
+    
+    
+    draw_merger_ycov2 <- function(draw_num = 1){
+      sim_data_draw <- sim_data %>% 
+        mutate(x1 = pv_x1.ycov[[draw_num]][,1] / sd(pv_x1.ycov[[draw_num]][,1]), 
+               x2 = pv_x2.ycov[[draw_num]][,1] / sd(pv_x2.ycov[[draw_num]][,1]))
+      return(sim_data_draw)
+    }
+    pv_imputes_ycov2 <- map(1:length(pv_x1.ycov), draw_merger_ycov2) %>%
+      imputationList()
+    mi_mods_ycov2 <- with(pv_imputes_ycov2,  lm(y ~ x1 + x2))
+    pv.mod.ycov2 <- MIcombine(mi_mods_ycov2)
+    
+    pv.temp.out <- data.frame(var = names(lm.mod.temp$coefficients),
+                              coefficient = pv.mod.ycov2$coefficients, 
+                              se = diag(sqrt(pv.mod.ycov2$variance)), 
+                              error = "PV: covariates", 
+                              estimator = "MI", 
+                              r.method = "None",
+                              reliability = NA)
+    pv.temp.out$true.cor.latent <- true.cors[pv.temp.out$var]
+    temp.sim.res[["pvycov2"]] <- pv.temp.out
+  }
+  
+  if(include.pv) {
+    bothdat <- cbind(x1dat, x2dat)
+    sim_data <- cbind(y, bothdat)
+    
+    pv_x1.simple <- fscores(x1.mod,  plausible.draws = 50)
+    pv_x2.simple <- fscores(x2.mod,  plausible.draws = 50)
+    
+    
+    draw_merger_simple <- function(draw_num = 1){
+      sim_data_draw <- sim_data %>% 
+        mutate(x1 = pv_x1.simple[[draw_num]][,1] / sd(pv_x1.simple[[draw_num]][,1]), 
+               x2 = pv_x2.simple[[draw_num]][,1] / sd(pv_x2.simple[[draw_num]][,1]))
+      return(sim_data_draw)
+    }
+    pv_imputes_simple <- map(1:length(pv_x1.simple), draw_merger_simple) %>%
+      imputationList()
+    mi_mods_simple <- with(pv_imputes_simple,  lm(y ~ x1 + x2))
+    pv.mod.simple <- MIcombine(mi_mods_simple)
+    
+    pv.temp.out <- data.frame(var = names(lm.mod.temp$coefficients),
+                              coefficient = pv.mod.simple$coefficients, 
+                              se = diag(sqrt(pv.mod.simple$variance)), 
+                              error = "PV: simple", 
+                              estimator = "MI", 
+                              r.method = "None",
+                              reliability = NA)
+    pv.temp.out$true.cor.latent <- true.cors[pv.temp.out$var]
+    temp.sim.res[["pvycov"]] <- pv.temp.out
+  }
+  
+  
+  
+  temp.sim.res <- do.call(rbind, temp.sim.res)
+  temp.sim.res <- temp.sim.res[temp.sim.res$var %in% c("x1", "x2"), ]
+  temp.sim.res$uci <- temp.sim.res$coefficient + 1.96 * temp.sim.res$se 
+  temp.sim.res$lci <- temp.sim.res$coefficient - 1.96 * temp.sim.res$se 
+  
+  temp.sim.res$true <- NA
+  temp.sim.res$true[temp.sim.res$var=="x1"] <- beta_1
+  temp.sim.res$true[temp.sim.res$var=="x2"] <- beta_2
+  
+  temp.sim.res$in95ci <- temp.sim.res$uci >temp.sim.res$true & temp.sim.res$lci <temp.sim.res$true
+  temp.sim.res$resid  <- temp.sim.res$coefficient - temp.sim.res$true
+  temp.sim.res$rx1x2 <- cor(cbind(x1,x2))[1,2]
+  temp.sim.res$sim <- sim
+  
+  temp.sim.res <- cbind(beta_1 = beta_1, beta_2 = beta_2, 
+                        x1.x2.beta = x1.x2.beta, 
+                        beta_cat = beta_cat,
+                        n.cats = n.cats, 
+                        n = n, 
+                        alpha = alpha, temp.sim.res)
+  
+  return(temp.sim.res)
+}
+
+library(future)
+library(future.apply)
+plan(multisession)
+n.sims <- 100
+sims.per.round <- 10
+sim.sets <- as.list(rep(NA, n.sims / sims.per.round))
+
+for(kk in which(is.na(sim.sets))) {
+  simout <- future_lapply(((kk-1)*sims.per.round) + (1:sims.per.round), simMIEIV, 
+                          alpha = 1,
+                          beta_1 = 1,
+                          beta_2 = 1,
+                          n = 2000,
+                          n.cats = 2,
+                          beta_cat = 0.5,
+                          x1.x2.beta = 0.5)
+  sim.sets[[kk]] <- simout
+}
+  
+
+sim.sets2 <- lapply(sim.sets[!is.na(sim.sets)], function(x) do.call(rbind, x))
+
+sim.all <- do.call(rbind, sim.sets2)
+
+sim.all$abs.resid <- abs(sim.all$resid)
+sim.all$resid2 <- sim.all$resid^2
+sim.all$true.reliability <- sim.all$true.cor.latent^2
+
+sim.all$resid.reliability <- sim.all$reliability - sim.all$true.reliability
+sim.all$abs.resid.reliability <- abs(sim.all$reliability - sim.all$true.reliability)
+sim.all$sig <- sim.all$lci>0
+sim.all$ci.width <- sim.all$uci - sim.all$lci
+
+sim.summary <- aggregate(sim.all[, c("in95ci", "resid", "abs.resid", "coefficient", 
+                                     "reliability", "resid.reliability", 
+                                     "abs.resid.reliability",
+                                     "resid2", "ci.width", "rx1x2")], sim.all[, c("var", "error", "estimator", "r.method")], 
+                         FUN = mean)
+sim.summary$rmse <- sqrt(sim.summary$resid2)
+sim.summary$resid2 <- NULL
+
+sim.summary$ratio <- NA
+sim.summary$ratio[sim.summary$var=="x1"] <- sim.summary$coefficient[sim.summary$var=="x1"] / sim.summary$coefficient[sim.summary$var=="x2"]
+sim.summary <- sim.summary[!(sim.summary$error=="classical" & sim.summary$r.method %in% c("marginal", "empirical")), ]
+sim.summary$n.sims <- sum(!is.na(sim.sets)) * sims.per.round
+library(dplyr)
+sim.summary <- sim.summary %>% arrange(estimator, desc(error), r.method, var)
+sim.summary <- sim.summary[(grepl("irt\\.cool|PV:|classical$", sim.summary$error)  & 
+                             sim.summary$estimator %in% c("EIV", "MI")) |
+                             (sim.summary$error=="irt" & sim.summary$estimator=="OLS") , ]
+
+sim.summary %>% 
+  mutate_if(is.numeric, round, 5) %>% tibble()
+
+
+save(sim.all, file = "resources/irt_eiv_sims100.rda")
---
+++ drafts/simulating_eiv_rosetta.R
@@ -0,0 +1,160 @@
+set.seed(43)
+n.sims <- 100
+library(mirt)
+library(mitools)
+library(tidyverse)
+alpha <- 1
+beta_1 <- 1
+beta_2 <- 1
+n <- 2000
+n.cats <- 2
+beta_cat <- 0.5
+x1.x2.beta <- 0.5
+
+include.pv <- TRUE
+sim.out <- as.list(rep(NA, n.sims)) 
+
+n.years <- 5
+x1.items.per.year <- 4
+total.items <- 10
+n.per.survey <- 2000
+max.x <- 1.5
+max.x1 <- max.x
+max.x2 <- max.x
+
+
+x1.means <- runif(n= (n.years+1), min = -max.x1, max = max.x1)
+x2.means <- runif(n= (n.years+1), min = -max.x2, max = max.x2)
+item.subsets <- t(replicate(n.years, (1:total.items) %in% 
+                              sample(1:total.items, size = x1.items.per.year, replace = F)))
+item.subsets <- rbind(item.subsets, TRUE)
+
+beta1.by.year <- runif(n= (n.years+1), min = -1, max = 1)
+beta2.by.year <- runif(n= (n.years+1), min = -1, max = 1)
+
+surveys <- c(LETTERS[1:n.years], "rosetta")
+
+names(beta1.by.year) <- names(beta2.by.year) <- rownames(item.subsets) <- 
+  names(x1.means) <- names(x2.means) <- 
+  surveys
+# generate x1 by year
+
+
+survey.ids <- inverse.rle(list(values = surveys, lengths = rep(n.per.survey, length(x1.means))))
+ 
+x1 <- rnorm(mean = x1.means[survey.ids], sd = 1, n = length(survey.ids))
+x2 <- rnorm(mean = x2.means[survey.ids], sd = 1, n = length(survey.ids)) + x1.x2.beta * x1
+x1 <- scale(x1)[, ]
+x2 <- scale(x2)[, ]
+
+y <- (beta1.by.year[survey.ids] * x1)  + (beta2.by.year[survey.ids] * x2) + rnorm(length(x1))
+
+item.parameters <- cbind(a = runif(min = 0.5, max = 2, total.items), b = runif(min = -2, max = 2, total.items))
+
+generateItemResponses <- function(item) {
+  rbinom(prob = plogis(item.parameters[item, "a"] * (x1 - item.parameters[item, "b"])), size = 1, n = length(x1))
+}
+all.responses <- sapply(1:total.items, generateItemResponses)
+all.responses[!item.subsets[survey.ids, ]] <- NA
+all.responses <- data.frame(all.responses)
+
+library(mirt)
+x1.mod <- mirt(data = data.frame(all.responses), model = 1)
+x1.hat <- fscores(x1.mod, full.scores = TRUE, full.scores.SE = TRUE)
+
+marginal_rxx2 <- function (mod, density = dnorm, var_theta = 1, which.items = 1:extract.mirt(mod, "nitems"), ...) 
+{
+  stopifnot(extract.mirt(mod, "nfact") == 1L)
+  stopifnot(is(mod, "SingleGroupClass"))
+  fn <- function(theta, mod, den, which.items, ...) {
+    TI <- testinfo(mod, matrix(theta), which.items = which.items)
+    TI/(TI + var_theta) * den(theta, ...)
+  }
+  integrate(fn, lower = -Inf, upper = Inf, mod = mod, den = density, which.items,
+            ...)$value
+}
+
+# sd(x1[surveys=="rosetta"])
+sd(x1)
+
+marginal.reliabilities <- sapply(surveys, function(x) marginal_rxx2(mod = x1.mod, which.items = which(item.subsets[surveys==x, ])) )
+empirical.reliabilities <- sapply(surveys, function(x) empirical_rxx(x1.hat[survey.ids==x, ]))
+true.reliabilities <- sapply(surveys, function(x) cor(cbind(x1[survey.ids==x], x1.hat[survey.ids==x, ]))[1,2])^2
+names(marginal.reliabilities) <- names(true.reliabilities) <- names(empirical.reliabilities) <- surveys
+library(eivtools)
+
+data <-  data.frame(survey = survey.ids,
+                    x1 = x1.hat[, 1], x2 = x2, y = y)
+
+rhoToS <- function(rho) {
+  s <- sqrt((1 / rho)^2 - 1)  
+  return(s)
+}
+
+eivIRTForSurvey <- function(current.survey, reliabilities) {
+  reliability <- reliabilities[current.survey]
+  data.temp <- data[data$survey==current.survey, ]  
+  data.temp$x1 <- data.temp$x1 / sd(data.temp$x1) * sqrt((1+rhoToS(sqrt(reliability))^2))
+  eirt.mod.temp <- eivreg(data = data.temp, 
+                               formula = y~x1+x2, 
+                               reliability = c(x1 = as.vector(reliability)))  
+  eirt.mod.sum <- summary(eirt.mod.temp)
+  coefs.to.extract <- c("x1", "x2")
+  out <- data.frame(estimator = "EIV",
+                    measurement = "IRT",
+                    var = coefs.to.extract, 
+             coef.est = eirt.mod.sum$coefficients[coefs.to.extract, c("Estimate")], 
+             se = eirt.mod.sum$coefficients[coefs.to.extract, c("Std. Error")], 
+             coef.true = c(beta1.by.year[current.survey],  beta2.by.year[current.survey]),
+             reliability.est = c(reliability, 1), 
+             reliablity.true = c((cor(data.temp$x1, x1[data$survey==current.survey]))^2, 1), 
+             survey = current.survey,
+             x1x2cor = cor(x2[data$survey==current.survey], x1[data$survey==current.survey]))
+  return(out)
+}
+
+all.eirt.for.rxx <- function(reliabilities) {
+  all.eirt.results <- lapply(surveys, eivIRTForSurvey, reliabilities = reliabilities)
+  eirt.comb <- do.call(rbind, all.eirt.results)
+  eirt.comb$uci <- eirt.comb$coef.est + 1.96 * eirt.comb$se
+  eirt.comb$lci <- eirt.comb$coef.est - 1.96 * eirt.comb$se
+  
+  eirt.comb$coef.true.mag <- (eirt.comb$coef.true * sign(eirt.comb$coef.true))
+  eirt.comb$coef.est.mag <- (eirt.comb$coef.est * sign(eirt.comb$coef.true))
+  
+  eirt.comb$resid.coef.mag <- eirt.comb$coef.est.mag - eirt.comb$coef.true.mag
+  eirt.comb$resid.coef <- eirt.comb$coef.est - eirt.comb$coef.true
+  
+  return(eirt.comb)
+}
+
+eirt.comb.true.rxx <- all.eirt.for.rxx(reliabilities = true.reliabilities)
+eirt.comb.emp.rxx <-  all.eirt.for.rxx(reliabilities = empirical.reliabilities)
+eirt.comb.marg.rxx <-  all.eirt.for.rxx(reliabilities = marginal.reliabilities)
+eirt.comb.true.rxx$reliability.method <- 'true'
+eirt.comb.emp.rxx$reliability.method <- 'empirical'
+eirt.comb.marg.rxx$reliability.method <- 'marginal'
+library(ggplot2)
+
+eirt.comb<- rbind(eirt.comb.true.rxx, eirt.comb.emp.rxx, eirt.comb.marg.rxx)
+
+eirt.comb$in95ci <- eirt.comb$coef.est >eirt.comb$lci & eirt.comb$coef.est <eirt.comb$uci
+eirt.comb$num.indicators <- factor(ifelse(eirt.comb$survey=="rosetta", 10, 4))
+# eirt.comb.marg.rxx
+
+library(gridExtra)
+library(mellonMisc)
+
+eirt.validation.plot <- ggplot(eirt.comb, aes(x = coef.true, y = coef.est, colour = var,
+                                                            shape = num.indicators)) +
+  geom_point() + xlab("True coefficient") +
+  facet_grid(cols = vars(reliability.method)) + ylab("Estimated coefficient") + 
+  ylim(c(-1,1)) + xlim(c(-1,1)) + geom_errorbar(aes(ymin = coef.true,ymax = coef.est), size=0.5) + 
+  geom_abline(size = 0.25, linetype = 2) + theme_bw()
+
+eirt.validation.plot
+
+within.sd <- mean(tapply(x1, survey.ids, sd)) 
+between.sd <- sd(tapply(x1, survey.ids, mean))
+# within.sd.prop = 
+(within.sd / (within.sd + between.sd))
